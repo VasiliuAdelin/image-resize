ae103602c19a10a6d5d354dc1aad00f375c87465
adelin <vasiliu.gheorghe.adelin@gmail.com>
2024-05-22 23:17:16 +0300

    insights

--- cloud-computing-project/Program.cs
+++ cloud-computing-project/Program.cs
@@ -1,10 +1,4 @@
 using cloud_computing_project.Models;
-using Microsoft.ApplicationInsights.Extensibility;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,16 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.Configure<AzureStorageConfig>(builder.Configuration.GetSection("AzureStorageConfig"));
 
+builder.Services.AddApplicationInsightsTelemetry();
+builder.Services.AddMvc();
+
 var app = builder.Build();
 
-// Configure Application Insights telemetry
-app.UseApplicationInsightsTelemetry();
-
-// Configure the Application Insights service options
-app.Services.ConfigureTelemetryModule<RequestTrackingTelemetryModule>((module, options) =>
-{
-    options.InstrumentationKey = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"];
-});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
